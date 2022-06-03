991e469d6ca29c6cea1a44d4a75f54220e86b716
yafengxn <feng.xiaoning@immomo.com>
2022-06-03 10:58:50 +0800

    Initial Commit

---
+++ PythonLite/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PythonLite
+//
+//  Created by Feng on 2022/6/3.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
